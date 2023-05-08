0065e2b93e1cd9da18ad7daf6695df0923e1ef16
Ser123rus <sergeyford13@gmail.com>
2023-05-08 21:46:00 +0300

    initial

--- AUV_light_reg/AUV_light_reg.cpp
+++ AUV_light_reg/AUV_light_reg.cpp
@@ -1,17 +1,110 @@
-﻿// AUV_light_reg.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение про#include <iostream>
+﻿#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
 
-int main()
+using namespace cv;
+using namespace std;
+
+Mat frame, src;
+VideoCapture cap(0); //open video for reading
+bool bSuccess;
+
+double cap_read()
 {
-    std::cout << "Hello World!\n";
+	if (cap.isOpened() == false) // if not success, exit program
+	{
+		cout << "Cannot open the video file" << endl;
+		cin.get(); //wait for any key press
+		return -1;
+	}
+
+	String window_name = "video";
+	namedWindow(window_name, WINDOW_NORMAL); //create a window
+
+	while (true)
+	{
+		bSuccess = cap.read(frame); // read a new frame from video 
+		if (bSuccess == false) 		//Breaking the while loop at the end of the video
+		{
+			cout << "Found the end of the video" << endl;
+			break;
+		}
+
+		imshow(window_name, frame); //show the frame in the created window
+
+		if (waitKey(10) == 27)
+		{
+			cout << "Esc key is pressed by user. Stoppig the video" << endl;
+			break;
+		}
+	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+Mat RGB2HSV(Mat src)
+{
+	int row = src.rows; int col = src.cols;
+	Mat dst(row, col, CV_32FC3);
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			float b = src.at<Vec3b>(i, j)[0] / 255.0;
+			float g = src.at<Vec3b>(i, j)[1] / 255.0;
+			float r = src.at<Vec3b>(i, j)[2] / 255.0;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+			float minn = min(r, min(g, b));
+			float maxx = max(r, max(g, b));
+
+			dst.at<Vec3f>(i, j)[2] = maxx; //V
+			float delta = maxx - minn; float h, s;
+
+			if (maxx != 0)
+			{
+				s = delta / maxx;
+			}
+			else
+			{
+				s = 0;
+			}
+			if (r == maxx)
+			{
+				h = (g - b) / delta;
+			}
+			else if (g == maxx)
+			{
+				h = 2 + (b - r) / delta;
+			}
+			else if (b == maxx)
+			{
+				h = 4 + (r - g) / delta;
+			}
+			else
+			{
+				h = 0;
+			}
+			h *= 60;
+			if (h < 0)
+				h += 360;
+			dst.at<Vec3f>(i, j)[0] = h; dst.at<Vec3f>(i, j)[1] = s;
+		}
+	}
+	return dst;
+}
+
+int main(int argc, char* argv[])
+{
+	cap_read();
+
+	src = cap.read(frame);
+	if (src.empty())
+	{
+		return -1;
+	}
+
+	Mat dst, dst1, dst2;
+	cvtColor(src, dst1, RGB2HSV);
+	dst = RGB2HSV(src);
+	return 0;
+}
